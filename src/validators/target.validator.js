c28e8886366a11711f4031009d23de60c4a44a6d
asad khan <asad.khan@simming.ai>
2026-04-25 15:19:19 +0500

    updated conditions

--- src/validators/target.validator.js
+++ src/validators/target.validator.js
@@ -3,8 +3,15 @@ const Joi = require('joi');
 const createTargetSchema = Joi.object({
   medicalRepId: Joi.string().required(),
   month: Joi.string().required().pattern(/^\d{4}-\d{2}$/),
-  salesTarget: Joi.number().required().min(0),
-  packsTarget: Joi.number().integer().required().min(0)
+  salesTarget: Joi.number().min(0).default(0),
+  packsTarget: Joi.number().integer().min(0).default(0)
+}).custom((value, helpers) => {
+  const sales = Number(value.salesTarget) || 0;
+  const packs = Number(value.packsTarget) || 0;
+  if (sales > 0 || packs > 0) return value;
+  return helpers.error('any.custom', {
+    message: 'At least one of sales target or packs target must be greater than 0'
+  });
 });
 
 const updateTargetSchema = Joi.object({
